872fe8208b0fb6a2d08f72da4b1678ebe7c2a22d
EonRitz <ronerintensiv@gmail.com>
2023-10-23 04:35:48 +0200

    More update

--- minerva.py
+++ minerva.py
@@ -1,6 +1,8 @@
 import pyttsx3 # pip install pyttsx3
 import datetime
 import speech_recognition as sr # pip install SpeechRecognition
+import wikipedia # pip install wikipedia
+import smtplib
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -56,6 +58,15 @@ def takeCommand():
         return "None"
     return query
 
+def sendmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('r.chichirita@gamil.com', 'Remuneration')
+    server.sendmail('r.chichirita@gmail.com', to, content)
+    server.close()
+
+
 if __name__ == "__main__":
     wishme()
     while True:
@@ -65,3 +76,23 @@ if __name__ == "__main__":
             time()
         elif 'date' in query:
             date()
+        elif 'wikipedia' in query:
+            speak('Searching...')
+            query = query.replace("wikipedia", "")
+            result = wikipedia.summary(query, sentences = 2)
+            print(result)
+            speak("According to Wikipedia...")
+            speak(result)
+        elif 'send email' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = 'roner@hotmail.ph'
+                sendmail(to, content)
+                speak("Email has been sent.")
+            except Exception as e:
+                print(e)
+                speak("Unable to end the email")
+
+        elif 'offline' in query:
+            quit()
